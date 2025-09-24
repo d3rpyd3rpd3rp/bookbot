ea909d6c2aedb486bf5d501c11e9cbc275130625
d3rpyd3rpd3rp <brandon.squires429@gmail.com>
2025-09-24 11:31:01 -0400

    stats.py now additionally calculates the number of characters

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_count
+from stats import *
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -7,6 +7,8 @@ def get_book_text(path_to_file):
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     word_count = get_word_count(book_text)
+    character_count = get_character_count(book_text)
     print(f"Found {word_count} total words")
+    print(f"{character_count}")
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,11 @@
 def get_word_count(book_text):
     return len(book_text.split())
+
+def get_character_count(book_text):
+    book_text = book_text.lower()
+    character_count = {}
+    for character in book_text:
+        if character not in character_count:
+            character_count[character] = 0
+        character_count[character] += 1
+    return character_count
