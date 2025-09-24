1523c8cdb6f30239c8f40cd6925efed70662f78b
d3rpyd3rpd3rp <brandon.squires429@gmail.com>
2025-09-24 12:15:11 -0400

    main.py properly analyzes a hard-coded book

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ def main():
     book_text = get_book_text("books/frankenstein.txt")
     word_count = get_word_count(book_text)
     character_count = get_character_count(book_text)
-    print(f"Found {word_count} total words")
-    print(f"{character_count}")
+    sorted_dictionary = get_sorted_dictionary(character_count)
+    print(f"============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...")
+    print(f"----------- Word Count ----------\nFound {word_count} total words")
+    print("--------- Character Count -------")
+    for char in sorted_dictionary:
+        print(f"{char["char"]}: {char["num"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -4,8 +4,20 @@ def get_word_count(book_text):
 def get_character_count(book_text):
     book_text = book_text.lower()
     character_count = {}
-    for character in book_text:
-        if character not in character_count:
-            character_count[character] = 0
-        character_count[character] += 1
+    for char in book_text:
+        if char not in character_count:
+            character_count[char] = 0
+        character_count[char] += 1
     return character_count
+
+def sort_on(items):
+    return items["num"]
+
+def get_sorted_dictionary(character_count):
+    chars = []
+    for k, v in character_count.items():
+        if not k.isalpha():
+            continue
+        chars.append({"char" : k, "num" : v})
+    chars.sort(reverse=True, key=sort_on)
+    return chars
